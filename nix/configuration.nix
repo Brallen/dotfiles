39385d643b5951b64326da388dce4ebca4ce2ffe
Brett Case <brett.case97@gmail.com>
2024-04-21 11:52:33 -0700

    22 current  2024-04-21 11:52:30  23.11.6478.bc194f70731c  6.1.87                          *

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -9,6 +9,7 @@
   imports = [
     # Include the results of the hardware scan.
     /etc/nixos/hardware-configuration.nix
+    ./modules/desktop.nix
     ./modules/languages.nix
     ./modules/terminal.nix
     ./modules/unfree.nix
