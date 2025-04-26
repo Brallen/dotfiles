cc46ec92007d777811cac2df7fa3216e699835b4
Brett Case <brett.case97@gmail.com>
2025-04-26 16:49:44 -0700

    65 current  2025-04-26 16:25:46  24.11.20250424.5630cf1  6.6.87                          *

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -67,7 +67,7 @@
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   # Enable the X11 windowing system.
