6a601556259b270da345a42c2cfc766fc751a75e
Brett <24304042+Brallen@users.noreply.github.com>
2026-06-06 16:42:45 -0700

    updating nix version

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,7 +2,7 @@
   description = "Ramp Rat Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
